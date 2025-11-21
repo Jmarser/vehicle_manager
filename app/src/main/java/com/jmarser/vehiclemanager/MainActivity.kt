ee4dea94f06698c1eb924656353b3ef51cc99e9a
Jmarser <juanmarser@signlab.info>
2025-11-21 13:40:32 +0100

    feat(ui): implement custom Poppind font family and base typography styles

--- app/src/main/java/com/jmarser/vehiclemanager/MainActivity.kt
+++ app/src/main/java/com/jmarser/vehiclemanager/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.stylusHoverIcon
 import androidx.compose.ui.tooling.preview.Preview
 import com.jmarser.vehiclemanager.ui.theme.VehicleManagerTheme
 
@@ -32,13 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = MaterialTheme.typography.displayLarge
     )
+        Text(
+            text = "Nuevo Texto",
+            style = MaterialTheme.typography.bodyLarge
+        ) }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     VehicleManagerTheme {
