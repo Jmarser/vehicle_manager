f3c92a0c1394ac3318006a60241f452f7077679f
Jmarser <juanmarser@signlab.info>
2025-11-21 22:06:27 +0100

    feat(ui): Implement reusable spacer components linked to responsive dimensions

--- app/src/main/java/com/jmarser/vehiclemanager/MainActivity.kt
+++ app/src/main/java/com/jmarser/vehiclemanager/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -22,7 +20,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.jmarser.vehiclemanager.core.presentation.ui.appDimens
+import com.jmarser.vehiclemanager.core.presentation.component.VerticalSpaceLarge
+import com.jmarser.vehiclemanager.core.presentation.component.VerticalSpaceNormal
 import com.jmarser.vehiclemanager.core.presentation.ui.getSizeForPhone
 import com.jmarser.vehiclemanager.core.presentation.ui.getSizeForTablet
 import com.jmarser.vehiclemanager.ui.theme.VehicleManagerTheme
@@ -62,12 +61,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier,
             style = MaterialTheme.typography.displayLarge
         )
-        Spacer(Modifier.height(appDimens.spacerXXL))
+        VerticalSpaceNormal()
         Text(
             text = "Nuevo Texto",
             style = MaterialTheme.typography.bodyLarge
         )
-        Spacer(Modifier.height(appDimens.spacerXXL))
+        VerticalSpaceNormal()
         Button(
             modifier = Modifier.padding(top = 24.dp),
             onClick = {}
