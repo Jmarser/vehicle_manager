51722d5e24f7bcf4bdd4ad8abbc6b35b9adf17df
Jmarser <juanmarser@signlab.info>
2025-11-24 09:27:22 +0100

    feat(ui): implement app branding, system colors light/dark and app icon

--- app/src/main/java/com/jmarser/vehiclemanager/ui/theme/Color.kt
+++ app/src/main/java/com/jmarser/vehiclemanager/ui/theme/Color.kt
@@ -2,10 +2,54 @@ package com.jmarser.vehiclemanager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// --- PALETA DE COLORES "VEHICLE MANAGER" ---
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val light_primary = Color(0xFF00668B)
+val light_onPrimary = Color(0xFFFFFFFF)
+val light_primaryContainer = Color(0xFFC3E7FF) // Azul muy suave
+val light_onPrimaryContainer = Color(0xFF001E2D)
+
+val dark_primary = Color(0xFF7CD0FF)
+val dark_onPrimary = Color(0xFF00344A)
+val dark_primaryContainer = Color(0xFF004C6A)
+val dark_onPrimaryContainer = Color(0xFFC3E7FF)
+
+val light_secondary = Color(0xFF4E616D)
+val light_onSecondary = Color(0xFFFFFFFF)
+val light_secondaryContainer = Color(0xFFD1E5F4)
+val light_onSecondaryContainer = Color(0xFF0A1E28)
+
+val dark_secondary = Color(0xFFB5C9D7)
+val dark_onSecondary = Color(0xFF20333E)
+val dark_secondaryContainer = Color(0xFF364955)
+val dark_onSecondaryContainer = Color(0xFFD1E5F4)
+
+val light_tertiary = Color(0xFF006C51) // Verde menta oscuro para contraste
+val light_onTertiary = Color(0xFFFFFFFF)
+val light_tertiaryContainer = Color(0xFF8DF8CD) // Menta vibrante (fondo)
+val light_onTertiaryContainer = Color(0xFF002117)
+
+val dark_tertiary = Color(0xFF70DBB1) // Menta pastel
+val dark_onTertiary = Color(0xFF003829)
+val dark_tertiaryContainer = Color(0xFF00513C)
+val dark_onTertiaryContainer = Color(0xFF8DF8CD)
+
+val light_error = Color(0xFFBA1A1A)
+val light_errorContainer = Color(0xFFFFDAD6)
+val light_onError = Color(0xFFFFFFFF)
+val light_onErrorContainer = Color(0xFF410002)
+
+val dark_error = Color(0xFFFFB4AB)
+val dark_errorContainer = Color(0xFF93000A)
+val dark_onError = Color(0xFF690005)
+val dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val light_background = Color(0xFFFBFCFE) // Casi blanco
+val light_onBackground = Color(0xFF191C1E) // Casi negro
+val light_surface = Color(0xFFFBFCFE)
+val light_onSurface = Color(0xFF191C1E)
+
+val dark_background = Color(0xFF191C1E) // Gris muy oscuro (mejor que negro puro)
+val dark_onBackground = Color(0xFFE1E2E5)
+val dark_surface = Color(0xFF191C1E)
+val dark_onSurface = Color(0xFFE1E2E5)
